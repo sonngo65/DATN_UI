0198c272559b82e8a29dba29108cde69e55a83a6
sonngo65 <ssngotat@gmail.com>
2024-04-03 18:17:28 +0700

    update relate-product to product detail

---
+++ src/Page/CartDetailPage.tsx
@@ -0,0 +1,15 @@
+import CartDetail from "../component/user/Cart/CartDetail";
+import CartDetailList from "../component/user/Cart/CartDetailList";
+import { Footer } from "../component/user/Footer";
+import Header from "../component/user/Header";
+import GiftInfo from "../component/user/ProductDetail/GiftInfo";
+
+export default function CartDetailPage() {
+  return (
+    <>
+      <Header />
+      <CartDetail />
+      <Footer />
+    </>
+  );
+}
---
+++ src/component/user/Product/component/RelatedProduct/RelatedProductItem.tsx
@@ -0,0 +1,38 @@
+import { FaRegStar } from "react-icons/fa";
+import "./_related-product.scss";
+interface RelatedProductItemProps {
+  img: string;
+  name: string;
+  rating: number;
+  price: number;
+  compareToPrice: number;
+}
+export default function RelatedProductItem({
+  img,
+  name,
+  rating,
+  price,
+  compareToPrice,
+}: RelatedProductItemProps) {
+  return (
+    <a href="#" className="related-product-li">
+      <div className="related-product-img">
+        <img src={img} alt={name} />
+      </div>
+      <div className="related-product-info">
+        <div className="name">{name}</div>
+        <div className="rating">
+          <FaRegStar />
+          <FaRegStar />
+          <FaRegStar />
+          <FaRegStar />
+          <FaRegStar />
+        </div>
+        <div className="price">
+          <strong>{price}</strong>{" "}
+          <span className="strike">{compareToPrice}</span>
+        </div>
+      </div>
+    </a>
+  );
+}
